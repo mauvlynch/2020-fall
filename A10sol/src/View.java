d2e483005cae6a9a35e24c95204960130d55c835
mauvlynch <70911419+mauvlynch@users.noreply.github.com>
2020-12-30 13:52:01 -0500

    Add files via upload

---
+++ A10sol/src/View.java
@@ -0,0 +1,33 @@
+/**
+ * @author Maureen Lynch
+ * @version 1.0
+ * @date Fall 2020
+ */
+import java.awt.*;
+import javax.swing.*;
+
+public class View extends JPanel {
+    private final int DIM = 800;
+    private final Game game;
+
+    public View(Game game, int delay) {
+        this.game = game;
+
+        setDoubleBuffered(true);
+
+        Timer timer = new Timer(delay, e -> {
+            game.step();
+            repaint();
+        });
+        timer.start();
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(DIM, DIM);
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        game.draw((Graphics2D)g, DIM);
+    }
+}
